d4d1af8485e5a3ef6825af9ebe10ad7c781e8c52
James Iter <james.iter.cn@gmail.com>
2018-12-08 05:40:53 +0800

    以 rpm 包的方式安装 JimV-N

--- INSTALL.sh
+++ INSTALL.sh
@@ -25,11 +25,6 @@ export LIBGUESTFISH_FILENAME=`basename ${LIBGUESTFISH_URL}`
 export LIBGUESTFISH_DIRNAME=`basename -s .tar.gz ${LIBGUESTFISH_FILENAME}`
 export SHOW_WARNING_VTX=false
 
-if [ ${COUNTRY} = 'CN' ]; then
-    export JIMVN_REPOSITORY_URL=${JIMVN_REPOSITORY_URL_CN}
-    export LIBGUESTFISH_URL=${LIBGUESTFISH_URL_CN}
-fi
-
 ARGS=`getopt -o h --long redis_host:,redis_password:,redis_port:,version:,help -n 'INSTALL.sh' -- "$@"`
 
 eval set -- "${ARGS}"
@@ -74,7 +69,7 @@ function check_precondition() {
     source /etc/os-release
     case ${ID} in
     centos|fedora|rhel)
-        if [ ${VERSION_ID} -lt 7 ]; then
+        if [[ ${VERSION_ID} -lt 7 ]]; then
             echo "系统版本号必须大于等于 7，检测到当前的系统版本号为 ${VERSION_ID}."
 	        exit 1
         fi
@@ -85,62 +80,60 @@ function check_precondition() {
         ;;
     esac
 
-    if [ `egrep -c '(vmx|svm)' /proc/cpuinfo` -eq 0 ]; then
+    if [[ `egrep -c '(vmx|svm)' /proc/cpuinfo` -eq 0 ]]; then
         export SHOW_WARNING_VTX=true
     fi
 
-    if [ ! ${JIMV_VERSION} ] || [ ${#JIMV_VERSION} -eq 0 ]; then
+    if [[ ! ${JIMV_VERSION} ]] || [[ ${#JIMV_VERSION} -eq 0 ]]; then
         export JIMV_VERSION='master'
     fi
 
-    if [ ! ${REDIS_HOST} ] || [ ${#REDIS_HOST} -eq 0 ]; then
+    if [[ ! ${REDIS_HOST} ]] || [[ ${#REDIS_HOST} -eq 0 ]]; then
         echo "你需要指定参数 '--redis_host'"
         exit 1
     fi
 
-    if [ ! ${REDIS_PORT} ]; then
+    if [[ ! ${REDIS_PORT} ]]; then
         export REDIS_PORT='6379'
     fi
 
-    if [ ! ${REDIS_PSWD} ]; then
+    if [[ ! ${REDIS_PSWD} ]]; then
         export REDIS_PSWD=''
     fi
 
     yum install epel-release -y
     yum install redis -y
-    yum install python2-pip git net-tools bind-utils gcc python-dmidecode -y
-    pip install --upgrade pip -i ${PYPI}
-    pip install virtualenv -i ${PYPI}
+    yum install python-dmidecode -y
 
     # 代替语句 ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'
-    export SERVER_IP=`hostname -I`
+    SERVER_IP=`hostname -I`; export SERVER_IP=${SERVER_IP%% *}
     export SERVER_NETMASK=`ifconfig | grep ${SERVER_IP} | grep -Eo 'netmask ?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*'`
     export GATEWAY=`route -n | grep '^0.0.0.0' | awk '{ print $2; }'`
     export DNS1=`nslookup 127.0.0.1 | grep Server | grep -Eo '([0-9]*\.){3}[0-9]*'`
     export NIC=`ifconfig | grep ${SERVER_IP} -B 1 | head -1 | cut -d ':' -f 1`
     export HOST_NAME=`grep ${SERVER_IP} /etc/hosts | awk '{ print $2; }'`
-    export NODE_ID=`python -c "import hashlib, string; m = hashlib.md5(); m.update('${HOST_NAME}'); print string.atoi(m.hexdigest(), 16).__str__()[:16]"`
+    export NODE_ID=`python -c "with open('/etc/machine-id', 'r') as f: _str = f.read().strip(); import string; import hashlib; m = hashlib.md5(); m.update(_str); print int(string.atoi(m.hexdigest(), 16).__str__()[:16])"`
 
-    if [ 'x_'${HOST_NAME} = 'x_' ]; then
+    if [[ 'x_'${HOST_NAME} = 'x_' ]]; then
         echo "计算节点 IP 地址未在 /etc/hosts 文件中被发现。请完整安装、初始化 JimV-C 后，再安装 JimV-N。"
         exit 1
     fi
 
     REDIS_RESPONSE='x_'`redis-cli -h ${REDIS_HOST} -a ${REDIS_PSWD} -p ${REDIS_PORT} --raw ping`
 
-    if [ ${REDIS_RESPONSE} != 'x_PONG' ]; then
+    if [[ ${REDIS_RESPONSE} != 'x_PONG' ]]; then
         echo "Redis 连接失败，请检查参数 --redis_host, --redis_password, --redis_port 是否正确。"
         exit 1
     fi
 
     REDIS_RESPONSE='x_'`redis-cli -h ${REDIS_HOST} -a ${REDIS_PSWD} -p ${REDIS_PORT} --raw EXISTS ${GLOBAL_CONFIG_KEY}`
-    if [ ${REDIS_RESPONSE} = 'x_0' ]; then
+    if [[ ${REDIS_RESPONSE} = 'x_0' ]]; then
         echo "安装 JimV-N 之前，你需要先初始化 JimV-C。"
         exit 1
     fi
 
     REDIS_RESPONSE='x_'`redis-cli -h ${REDIS_HOST} -a ${REDIS_PSWD} -p ${REDIS_PORT} --raw HEXISTS ${GLOBAL_CONFIG_KEY} ${VM_NETWORK_KEY}`
-    if [ ${REDIS_RESPONSE} = 'x_0' ]; then
+    if [[ ${REDIS_RESPONSE} = 'x_0' ]]; then
         echo "未在 JimV-C 的配置中发现 key ${VM_NETWORK_KEY}，请重新配置 JimV-C。"
         exit 1
     else
@@ -148,7 +141,7 @@ function check_precondition() {
     fi
 
     REDIS_RESPONSE='x_'`redis-cli -h ${REDIS_HOST} -a ${REDIS_PSWD} -p ${REDIS_PORT} --raw HEXISTS ${GLOBAL_CONFIG_KEY} ${VM_NETWORK_MANAGE_KEY}`
-    if [ ${REDIS_RESPONSE} = 'x_0' ]; then
+    if [[ ${REDIS_RESPONSE} = 'x_0' ]]; then
         echo "未在 JimV-C 的配置中发现 key ${VM_NETWORK_MANAGE_KEY}，请重新配置 JimV-C。"
         exit 1
     else
@@ -156,23 +149,13 @@ function check_precondition() {
     fi
 
     REDIS_RESPONSE='x_'`redis-cli -h ${REDIS_HOST} -a ${REDIS_PSWD} -p ${REDIS_PORT} --raw HEXISTS ${HOSTS_INFO} ${NODE_ID}`
-    if [ ${REDIS_RESPONSE} != 'x_0' ]; then
+    if [[ ${REDIS_RESPONSE} != 'x_0' ]]; then
         echo "计算节点 ${HOST_NAME} 已存在，请重新指定 --hostname 的值，或清除冲突的计算节点。"
         exit 1
     else
         hostname ${HOST_NAME}
         echo ${HOST_NAME} > /etc/hostname
     fi
-}
-
-function set_ntp() {
-    yum install ntp -y
-    sed -i "/^server 0.centos.pool.ntp.org iburst/i\server ${REDIS_HOST} prefer" /etc/ntp.conf
-    systemctl start ntpd
-    systemctl enable ntpd
-    timedatectl set-timezone Asia/Shanghai
-    timedatectl set-ntp true
-    timedatectl status
 }
 
 function custom_repository_origin() {
@@ -198,23 +181,20 @@ function clear_up_environment() {
 
 function install_libvirt_and_libguestfish() {
     # 安装 libvirt
+    cat > /etc/yum.repos.d/JimV.repo << EOF
+[JimV]
+name=JimV - \$basearch
+baseurl=http://repo.iit.im/centos/7/os/\$basearch
+failovermethod=priority
+enabled=1
+gpgcheck=0
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-7
+EOF
+
     uname -m | grep -q 'x86_64'  && echo 'centos' >/etc/yum/vars/contentdir || echo 'altarch' >/etc/yum/vars/contentdir
-    yum install libvirt libvirt-devel python-devel centos-release-qemu-ev -y
-    yum install ocaml-findlib-devel ocaml-gettext-devel ocaml-ounit-devel ocaml-libvirt-devel ocaml-hivex-devel ocaml-ocamldoc -y
-    yum install hivex-devel python-hivex gperf genisoimage flex bison ncurses-devel pcre-devel augeas-devel supermin5 cpio xz -y
-    yum install libxml2 yajl-devel file-devel bash-completion fuse-devel python-devel gcc qemu-kvm-ev qemu seabios -y
-    yum install readline-devel libconfig-devel ntfs-3g-devel -y
-    ln -s /usr/bin/supermin5 /usr/bin/supermin
-    curl ${LIBGUESTFISH_URL} -o ${LIBGUESTFISH_FILENAME}
-    tar -xf ${LIBGUESTFISH_FILENAME}
-    rm -f ${LIBGUESTFISH_FILENAME}
-    cd ${LIBGUESTFISH_DIRNAME}
-    ./configure --disable-perl --disable-ruby --disable-haskell --without-java --disable-php --disable-erlang --disable-lua --disable-golang --disable-gobject
-    make -j${CPU_COUNT}
-    export REALLY_INSTALL=yes
-    make install
-    cd ..
-    rm -rf ${LIBGUESTFISH_DIRNAME}
+    yum install centos-release-qemu-ev -y
+    yum install jimv-node -y
+    yum install librbd1-10.2.5 -y
 }
 
 function handle_ssh_client_config() {
@@ -289,54 +269,12 @@ function start_libvirtd() {
     systemctl disable dnsmasq
     systemctl enable libvirtd
     systemctl start libvirtd
-    virsh net-destroy default; virsh net-undefine default
-}
-
-function clone_and_checkout_JimVN() {
-    git clone ${JIMVN_REPOSITORY_URL} /usr/local/JimV-N
-    if [ ! $? -eq 0 ]; then
-        echo '克隆 JimV-N 失败，请检查网络可用性。'
-        exit 1
-    fi
-
-    if [ ${JIMV_VERSION} != 'master' ]; then
-        cd /usr/local/JimV-N && git checkout ${JIMV_VERSION}
-    fi
-}
-
-function get_JimVN() {
-    mkdir -p /usr/local/JimV-N
-    curl -sL ${JIMVN_DOWNLOAD_URL} | tar -zxf - --strip-components 1 -C /usr/local/JimV-N
-}
-
-function install_dependencies_library() {
-    mkdir -p ~/.pip
-    cat > ~/.pip/pip.conf << EOF
-[global]
-index-url = ${PYPI}
-EOF
-
-    # 创建 python 虚拟环境
-    virtualenv --system-site-packages /usr/local/venv-jimv
-
-    # 导入 python 虚拟环境
-    source /usr/local/venv-jimv/bin/activate
-
-    # 自动导入 python 虚拟环境
-    echo '. /usr/local/venv-jimv/bin/activate' >> .bashrc
-
-    # 安装 JimV-N 所需扩展库
-    grep -v "^#" /usr/local/JimV-N/requirements.txt | xargs -n 1 pip install -i ${PYPI}
 }
 
 function generate_config_file() {
-    cp -v /usr/local/JimV-N/misc/jimvn.conf /etc/jimvn.conf
     sed -i "s/\"redis_host\".*$/\"redis_host\": \"${REDIS_HOST}\",/" /etc/jimvn.conf
     sed -i "s/\"redis_password\".*$/\"redis_password\": \"${REDIS_PSWD}\",/" /etc/jimvn.conf
     sed -i "s/\"redis_port\".*$/\"redis_port\": \"${REDIS_PORT}\",/" /etc/jimvn.conf
-
-    cp -v /usr/local/JimV-N/misc/jimvn.service /etc/systemd/system/jimvn.service
-    systemctl daemon-reload
 }
 
 function start_JimVN() {
@@ -350,7 +288,7 @@ function display_summary_information() {
     echo "==========="
     echo
 
-    if [ ${SHOW_WARNING_VTX} = true ]; then
+    if [[ ${SHOW_WARNING_VTX} = true ]]; then
         echo "警告：请检查 CPU 是否开启 VT 技术。未开启 VT 技术的计算节点，将以 QEMU 模式运行虚拟机。"
         echo
     fi
@@ -360,32 +298,15 @@ function display_summary_information() {
     echo
 }
 
-function is_installed {
-    if yum list installed "$@" >/dev/null 2>&1; then
-        true
-    else
-        false
-    fi
-}
-
 function deploy() {
     check_precondition
-    set_ntp
     custom_repository_origin
     clear_up_environment
     install_libvirt_and_libguestfish
-
-    if ! is_installed centos-release-qemu-ev; then
-      # 如果 centos-release-qemu-ev 没有被安装，则再给一次安装机会
-      install_libvirt_and_libguestfish
-    fi
-
     handle_ssh_client_config
     handle_net_bonding_bridge
     create_network_bridge_in_libvirt
     start_libvirtd
-    get_JimVN
-    install_dependencies_library
     generate_config_file
     start_JimVN
     display_summary_information
